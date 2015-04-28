ff8647d968c9716ad3d281ba51c642b61a6b0115
Mike Harms <harmsm@gmail.com>
2015-04-28 11:30:58 -0700

    Updated binding part so it now accurately reflects concentration.

--- simulate/sampling.py
+++ simulate/sampling.py
@@ -166,6 +166,64 @@ class BindingSampler(SamplerBaseClass):
     
         return pool_instance.current_affinities/np.sum(pool_instance.current_affinities)
     
+    def runExperiment(self,pool_instance,sample_size):
+        """
+        Run a sampling experiment.  
+        
+            Args: pool_instance (current pool)
+                  sample_size (number of sequences to take forward)
+        
+            Returns: new_contents (new sequences after sampling)
+                     new_counts (number of times each sequence is seen)
+        
+        """
+
+        # Make sure it even makes sense to run an experiment
+        self.poolSanityCheck(pool_instance)
+       
+        # Grab the total number of sequences seen 
+        total_counts = np.sum(pool_instance.current_counts)
+    
+        # Calculate probability that each sequence binds to a single site.
+        weights = pool_instance.current_affinities*pool_instance.current_counts
+        Q = 1 + np.sum(weights)
+        weights = weights/Q
+
+        # Calcualte probability that nothing binds at all.
+        p_nothing = 1/Q
+      
+        # Normalize the binding probability to the number of times we will query
+        # this probability in the final, expanded array
+        weights = weights/pool_instance.current_counts
+
+        # Add a "nothing-bound" probability to the weights.  This will be added 
+        # "total_counts" times, hence the normalization to total_counts.
+        weights = np.append(p_nothing/total_counts,weights)
+     
+        # Add a "nothing bound" to the possible contents 
+        contents = pool_instance.current_contents.copy()
+        contents = np.append(-1,contents)
+
+        # Add a total_counts "nothing bound" occurences to the counts 
+        counts = pool_instance.current_counts.copy()
+        counts = np.append(total_counts,counts)
+
+        # Create possibilities and weights arrays arrays by repeating these instances
+        # the number of times they occur.
+        possibilities = np.repeat(contents,counts)
+        weights = np.repeat(weights,counts)
+
+        # Sample                    
+        new_contents, new_counts = self._sample(possibilities,
+                                                weights=weights,
+                                                sample_size=sample_size)
+
+        # toss empties if seen
+        if new_contents[0] == -1:
+            return new_contents[1:], new_counts[1:] 
+
+        return new_contents, new_counts
+    
 
 class IlluminaRunSampler(SamplerBaseClass):
     pass
