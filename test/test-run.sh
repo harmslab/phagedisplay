ecb2934fecc3ad24267b042b37fe5bf060c14e8a
Mike Harms <harmsm@gmail.com>
2015-05-25 21:35:49 -0700

    finished stitching together pipeline between sims and experimental processor

--- test/test-run.sh
+++ test/test-run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# Should create a directory called "from-fastq-output" containing the processed experimental input
-../runPipeline.py from-fastq-output -f *.fastq -r rounds.txt -k key-file.txt
+echo "Testing fastq import"
+../runPipeline.py from-fastq-input -f *.fastq -r rounds.txt -k key-file.txt
+
+echo "Testing pickle import"
+../runPipeline.py from-pickle-input -p test.pickle
+
+echo "Testing simulation import (illumina reads)"
+../runPipeline.py from-sim-illumina-input -s output.simulation
+
+echo "Testing simulation import (all samples)"
+../runPipeline.py from-sim-all-input -s output.simulation -a
+
+echo "Test basic simulation"
+../runSimulation.py test-simulation
+
