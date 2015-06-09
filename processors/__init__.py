0ca7e76f79eaa401ac8fca1cbeea85c795c14326
Mike Harms <harmsm@gmail.com>
2015-06-09 16:28:02 -0700

    basic files added for new feature (illumina errors!)

--- processors/__init__.py
+++ processors/__init__.py
@@ -7,11 +7,12 @@ __author__ = "Michael J. Harms"
 __date__ = "2015-04-27"
 
 __all__ = ["base","master","fastq_list","fastq_to_counts","pickle_dict",
-           "regress_enrichment"]
+           "sequencing_error","regress_enrichment"]
 
 from .base import BaseProcessor
 from .master import MasterProcessor
 from .fastq_list import FastqListProcessor
 from .fastq_to_counts import FastqToCountsProcessor
 from .pickle_dict import PickleDictProcessor
+from .sequencing_error import SequencingErrorProcessor
 from .regress_enrichment import RegressEnrichmentProcessor
---
+++ processors/sequencing_error.py
@@ -0,0 +1,27 @@
+__description__ = \
+"""
+Use a maximum likelihood model to estimate the actual frequencies of clones in a
+library given that there are sequencing errors.  Follows Lynch paper.
+"""
+__author__ = "Michael J. Harms"
+__date__ = "2015-06-09"
+
+
+from . import BaseProcessor
+
+class SequencingErrorProcessor(BaseProcessor):
+    """
+    """
+
+    def process(self):
+        """
+        """
+
+        pass
+
+    @property
+    def data(self):
+        """
+        """
+
+        pass
