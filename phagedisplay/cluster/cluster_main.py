4b9abd104c5e0914418036ea134c4e95b65b06be
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2016-01-24 22:41:25 -0800

    testing modules together

--- phagedisplay/cluster/cluster_main.py
+++ phagedisplay/cluster/cluster_main.py
@@ -1,3 +1,5 @@
+import pyximport
+pyximport.install()
 from DistMatrix import *
 #from Cluster import *
 
@@ -7,7 +9,7 @@ from DistMatrix import *
 #clustering = input("input clustering algorithm: ")
 
 test = DistMatrix('hA5-0.0.txt', 'hamming', False).calcDistMatrix()
-print(test)
+self, phage_file, scoring = None, seq = None
 
 #cluster tests
 #Cluster(test, 'k_means', 3).silhouetteAnalysis()
