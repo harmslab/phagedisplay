60fbcd20a5c384b70e562caf96259d1679e075c8
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2016-02-17 18:58:21 -0800

    simplified

--- phagedisplay/cluster/setup.py
+++ phagedisplay/cluster/setup.py
@@ -1,17 +1,10 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import numpy as np
 
-extensions = Extension(
-    "DistMatrix",
-    ["DistMatrix.pyx"]
-)
 
 setup(
     name = 'DistMatrix',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = cythonize(extensions),
+    ext_modules = cythonize('DistMatrix.pyx'),
     include_dirs = [np.get_include()]
 )
