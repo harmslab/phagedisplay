3a74de8ba71e7a3aba1586fed6b0ad16060718c3
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2016-03-11 14:56:15 -0800

    formatting fixed, amino dict added. in progress

--- phagedisplay/cluster/DistMatrix.pyx
+++ phagedisplay/cluster/DistMatrix.pyx
@@ -8,12 +8,12 @@ import random
 
 cimport cython
 
-def readMatrix(fileName):
+def read_matrix(file_name):
     """
     read matrix text file into dictionary for scoring.
     """
 
-    data = open(fileName).readlines()
+    data = open(file_name).readlines()
     seq = data[0].strip('/n/r').split()
 
     matrix = {}
@@ -26,14 +26,14 @@ def readMatrix(fileName):
 
     return matrix
 
+
 cdef class DistMatrix:
     """
     Takes a file, makes a list of sequences, and computes a distance matrix.
     Distance matrix computed using 1 - exp(probability_score) with weighted scoring.
     
     args:
-            matrix: the biopython matrix to use for pairwise scoring. Can score using hamming distance 
-                    if matrix = 'hamming' or a given matrix.
+            matrix: distance scoring to use in calculating distance matrix between sequences.
             
             seq (optional): if not declared, assumes file is already a list of sequences. If seq = 'no' declared then
             list of sequence is pulled out from phage data file.
@@ -42,15 +42,29 @@ cdef class DistMatrix:
     """
     
     cdef str _phage_file, _seq
-    cdef _scoring, _matrix
+    cdef _scoring, _matrix, _amino_num
     
     def __init__(self, phage_file, scoring = None, seq = None):
         self._phage_file = phage_file
         self._scoring = scoring
         self._seq = seq
-        self._matrix = readMatrix('blosum62.txt')
+        self._matrix = read_matrix('blosum62.txt')
+        self._amino_num = {'G' : 0, 'A' : 1, 'V': 2, 'L' : 3, 'I' : 4, 'P' : 5, 'F' : 6, 'Y' : 7, 'W' : 8, 
+                           'E' : 9, 'D' : 10, 'K' : 11, 'R' : 12, 'H' : 13, 'S' : 14, 'T' : 15, 'C' : 16, 
+                           'M' : 17, 'N' : 18, 'Q' : 19}
         
-    cdef createSeqList(self, length):
+    
+    cpdef aminos_int(self, seq):
+        
+        int_seq = []
+        
+        for i in seq:
+            for j in i:
+                int_seq.append(self._amino_num[j])
+            
+        return int_seq
+        
+    cpdef create_list(self):
         """
         Creates a list of sequences from a file with a k_independent greater than 1.00
         If phage file is already a list of sequences, outputs that as a list.
@@ -64,7 +78,7 @@ cdef class DistMatrix:
                 for line in data:
                     num, seq, k_glob, theta_glob, k_ind, theta_ind = line.split()
                     phage_data.append(seq) if float(k_ind) > 1.000000000e+00 else None
-            return phage_data[:length]
+            return phage_data
         elif self._seq == 'yes':
             return [line.strip() for line in open(self._phage_file)]
         else:
@@ -72,7 +86,7 @@ cdef class DistMatrix:
     
     @cython.wraparound(False)
     @cython.boundscheck(False)
-    cdef double scorePairwise(self, seq1, seq2): 
+    cdef double score_pairwise(self, seq1, seq2): 
         """
         calculate score between two sequences using a given scoring matrix.
         """
@@ -96,12 +110,12 @@ cdef class DistMatrix:
     
     @cython.wraparound(False)
     @cython.boundscheck(False)
-    def calcDistMatrix(self, length):
+    def calc_matrix(self):
         """
         calculates pairwise distance between sequences in a given file and returns a distance matrix.
         """
         cdef:
-            np.ndarray sequences = np.array(self.createSeqList(length))
+            np.ndarray sequences = np.array(self.create_list())
             int i, j, nrow = sequences.shape[0]
             np.ndarray[dtype=double, ndim=2] D = np.zeros((nrow, nrow))
             
@@ -109,7 +123,7 @@ cdef class DistMatrix:
 
         for i in range(nrow):
             for j in range(i + 1, nrow):
-                temp = self.scorePairwise(sequences[i], sequences[j])
+                temp = self.score_pairwise(sequences[i], sequences[j])
                 D[i, j] = temp
                 D[j, i] = temp
                 
@@ -117,7 +131,7 @@ cdef class DistMatrix:
     
         return dist_matrix
 
-def genSeqList(N, M, l, q):
+def gen_list(l, file_name):
     """
     generate dummy data.
     
@@ -125,27 +139,33 @@ def genSeqList(N, M, l, q):
         N: number of clusters
         M: differences in each sequence
         l: length of sequences
-        q: minimum number of sequences in each cluster
+        q: max number of sequences in each cluster
     """
     
     aminos = ['G', 'A', 'V', 'L', 'I', 'P', 'F', 'Y', 'W', 'E', 'D', 'K', 'R', 'H', 'S', 'T', 'C', 'M', 'N', 'Q']
-    seqList = []
+    seq_list = []
+    
+    N = random.randint(50, 150)
+    M = random.randint(2, 7)
+    
+    file_save = {'# clusters': N, '# of differences' : M}
+    pickle.dump(file_save, open(file_name, 'wb'))
     
     for i in range(N):
         seq = [random.choice(aminos) for x in range(l)]
-        seqList.append(''.join(seq))
+        seq_list.append(''.join(seq))
         
         # randomizes number of sequences in each cluster.
-        randClustNum = random.randint(q, q+20)
-        for j in range(randClustNum):
+        rand_clust_num = random.randint(5, 250)
+        for j in range(rand_clust_num):
             seq2 = seq[:]
             for k in range(M):
                 index = random.randint(0, l-1)
                 seq2[index] = random.choice(aminos)
-            seqList.append(''.join(seq2))
+            seq_list.append(''.join(seq2))
         
-    return pd.DataFrame(seqList)
+    return pd.DataFrame(seq_list)
 
-def toFile(seqList, fileName):
+def to_file(seq_list, file_name):
     
-    seqList.to_csv(fileName, index = False, header = False)
+    seq_list.to_csv(file_name, index = False, header = False)
