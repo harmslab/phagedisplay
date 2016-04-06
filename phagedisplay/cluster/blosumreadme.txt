1bbf8a956eed63e1eb575b8d60ecea73a56a5f68
Mike Harms <harmsm@gmail.com>
2016-04-06 12:55:34 -0700

    refactor dist matrix calculation

--- phagedisplay/cluster/blosumreadme.txt
+++
@@ -1,6 +0,0 @@
-#  Matrix made by matblas from blosum62.iij
-#  * column uses minimum score
-#  BLOSUM Clustered Scoring Matrix in 1/2 Bit Units
-#  Blocks Database = /data/blocks_5.0/blocks.dat
-#  Cluster Percentage: >= 62
-#  Entropy =   0.6979, Expected =  -0.5209
--- phagedisplay/processors/__init__.py
+++ phagedisplay/processors/__init__.py
@@ -12,6 +12,7 @@ __all__ = ["base",
            "fastq_to_counts",
            "pickle_dict",
            #"sequencing_error",
+           #"cluster",
            "regress_enrichment"]
 
 from .base import BaseProcessor
@@ -20,4 +21,5 @@ from .fastq_list import FastqListProcessor
 from .fastq_to_counts import FastqToCountsProcessor
 from .pickle_dict import PickleDictProcessor
 #from .sequencing_error import SequencingErrorProcessor
+#from .cluster import ClusterProcessor
 from .regress_enrichment import RegressEnrichmentProcessor
